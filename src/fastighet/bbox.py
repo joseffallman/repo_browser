7fecdaaa4ebbd4e4024363d279c3b6765f52cc93
Josef Fällman <josef.fallman@gmail.com>
2025-07-13 12:55:27 +0000

    Update typehints

--- src/fastighet/bbox.py
+++ src/fastighet/bbox.py
@@ -1,11 +1,20 @@
+from typing import Tuple
+
 from pyproj import Transformer
 
 transformer = Transformer.from_crs("EPSG:4326", "EPSG:3006", always_xy=True)
 
 
-def degrees_to_meters(minx, miny, maxx, maxy) -> tuple(int, int):
-    """ Konverterar bbox-koordinater från grader till meter i EPSG:3006 """
-    # Transformera koordinaterna till EPSG:3006
+def degrees_to_meters(minx, miny, maxx, maxy) -> Tuple[int, int]:
+    """
+    Transform a bounding box from EPSG:4326 to EPSG:3006 and calculate its size in meters.
+
+    :param minx: Minimum longitude
+    :param miny: Minimum latitude
+    :param maxx: Maximum longitude
+    :param maxy: Maximum latitude
+    :return: Tuple containing (width in meters, height in meters)
+    """
     x1, y1 = transformer.transform(minx, miny)
     x2, y2 = transformer.transform(maxx, maxy)
 
